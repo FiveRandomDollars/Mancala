16611c8efd029b7a90fdb80e339cc21cb31d1a3a
125734523546462 <155582724+FiveRandomDollars@users.noreply.github.com>
2026-09-23 08:16:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # This is a sample Python script.
-from idlelib import sidebar
-from unittest import case
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
